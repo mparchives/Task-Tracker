8d768430d6c58651875dab3d7ef9f9c5745cecfd
Michelle Pickerel <michellepickerel@gmail.com>
2024-05-08 17:31:26 -0500

    5/8/24 changes

--- task.py
+++ task.py
@@ -1,24 +1,47 @@
-#this is just a test module
+from todo import Todo
+import datetime
 
-class Task():
+def greet():
+    print(f"Welcome back to the task-tracker! Today's date: {(datetime.date.today()).strftime("%m/%d/%y")}")
 
-    def __init__(self, id=0, topic = ""):
-        self.__id = id
-        self.__topic = topic
+def menu():
+    print("Choose Action:")
+    print("1. Print Today's Date")
+    print("2. Add Task to the List")
+    print("3. Show Tasks")
+    print("4. End This Program")
+    action = int(input("> "))
+    return action
 
-    #set/get taskID
-    def setID(self, taskID):
-        self.__id = taskID
-    
-    def getID(self):
-        return self.__id
-    
-    #set/get topic
-    def setTopic(self, topics):
-        self.__topic = topics
+def showToday():
+    print(f"Today's date: {(datetime.date.today()).strftime("%m/%d/%y")}")
 
-    def getTopic(self):
-        return self.__topic
-    
-    def __str__(self):
-        return f"{self.__id}.  {self.__topic}\n"
+
+def idTrack(seq, idlyst):
+    idlyst.append(seq)
+    return idlyst
+
+def addTask(taskNum, tasklyst):
+
+        
+    newCateg = input("Input category for the new task: >")
+    newTopic = input("Input topic for the new task: >")
+    newDes = input("Input description for the new task: >")
+    newdue = int(input("Input number of days from now the task is due: >"))
+    newStat = int(input("Input status for the new task: (0/1/2)>"))
+    temp = Todo(taskNum, newCateg, newTopic, newDes, newdue, newStat)
+
+    print(temp)
+    tasklyst.append(temp)
+
+    return tasklyst
+
+
+def showTask(tasklyst):
+
+    if len(tasklyst) == 0:
+        print("You have no task now.")
+
+    else:
+        for i in tasklyst:
+            print(i)

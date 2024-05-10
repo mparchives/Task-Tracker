81506a839cd37fa70a6b6ac4679b984af2c6822a
Michelle Pickerel <michellepickerel@gmail.com>
2024-05-09 21:48:38 -0500

    5/9/24 work

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-from task import greet, menu, showToday, idTrack, addTask, showTask
+from task import greet, menu, showToday, addTask, showTask, editTask, removeTask, noTask
 
 def main():
 
     greet()
 
-    seq = 0
-    tasklyst = []
-    idlyst = []
-        
+    seq = 0        #count amount of tasks
+    tasklyst = []  #store ongoing task objects 
+    archived = []  #store archived task objects
     flag_menu = True
+
     while (flag_menu != False):
 
         action = menu()
+
+        #1. Show Today's Date
         if action == 1:
             showToday()
-        
+
+        #2. Show Tasks
         elif action == 2:
+
+            #Check if there's task on the list
+            taskFlag = noTask(tasklyst)
+            if taskFlag == True:
+                print("You have no task now.")
+
+            else:
+                showTask(tasklyst)
+        
+        #3. Add Task to the List
+        elif action == 3:
 
             flag_add = True
             while( flag_add != False):
 
                 seq += 1
-                idlyst = idTrack(seq, idlyst)
-                tasklyst = addTask(seq, tasklyst)
+                temp = addTask(seq)
+                tasklyst.append(temp)
 
                 add = int(input("Keep adding task? Yes: 1, No: 2 >"))
 
@@ -32,10 +46,59 @@ def main():
                 else:
                     flag_add = False       
         
-        elif action == 3:
-            showTask(tasklyst)
-
+        #4. Edit Task on the List
         elif action == 4:
+
+            #Check if there's task on the list
+            taskFlag = noTask(tasklyst)
+            if taskFlag == True:
+                print("You have no task now.")
+
+            else:
+
+                editNum = int(input("Which task would you like to edit? >"))
+                if editNum <=0 or editNum > seq:
+                    print("Out of range!")
+
+                else:
+                    temp = addTask(editNum)
+                    tasklyst = editTask(editNum, temp, tasklyst)
+
+        #5. Remove Task on the List
+        elif action == 5:
+
+            #Check if there's task on the list
+            taskFlag = noTask(tasklyst)
+            if taskFlag == True:
+                print("You have no task now.")
+
+            else:
+
+                editNum = int(input("Which task would you like to edit? >"))
+                if editNum <=0 or editNum > seq:
+                    print("Out of range!")
+
+                else:
+
+                    for i in range(len(tasklyst)):
+                        if i == editNum-1:
+                            archived.append(tasklyst[i])
+                    
+                    tasklyst = removeTask(editNum, tasklyst)
+
+
+        elif action == 6:
+            
+            #Check if there's task on the list
+            taskFlag = noTask(archived)
+            if taskFlag == True:
+                print("No archived tasks on the list.")
+
+            else:
+                showTask(archived)
+
+        #6. End This Program
+        elif action == 7:
             flag_menu = False
 
         else:
--- task.py
+++ task.py
@@ -6,42 +6,69 @@ def greet():
 
 def menu():
     print("Choose Action:")
-    print("1. Print Today's Date")
-    print("2. Add Task to the List")
-    print("3. Show Tasks")
-    print("4. End This Program")
+    print("1. Show Today's Date")
+    print("2. Show Tasks")
+    print("3. Add Task to the List")
+    print("4. Edit Task on the List")
+    print("5. Remove Task on the List")
+    print("6. View Archieved Task")
+    print("7. End This Program")
     action = int(input("> "))
     return action
 
 def showToday():
     print(f"Today's date: {(datetime.date.today()).strftime("%m/%d/%y")}")
 
+def showTask(tasklyst):
 
-def idTrack(seq, idlyst):
-    idlyst.append(seq)
-    return idlyst
+    for i in tasklyst:
+        print(i)
 
-def addTask(taskNum, tasklyst):
+def addTask(taskNum):
 
-        
     newCateg = input("Input category for the new task: >")
     newTopic = input("Input topic for the new task: >")
     newDes = input("Input description for the new task: >")
     newdue = int(input("Input number of days from now the task is due: >"))
     newStat = int(input("Input status for the new task: (0/1/2)>"))
-    temp = Todo(taskNum, newCateg, newTopic, newDes, newdue, newStat)
+    temps = Todo(taskNum, newCateg, newTopic, newDes, newdue, newStat)
 
-    print(temp)
-    tasklyst.append(temp)
+    print(temps)
+    return temps
+
+
+def editTask(editNum, newTemp, tasklyst):
+
+    for i in range(len(tasklyst)):
+        if i == editNum-1:
+            tasklyst[i] = newTemp
 
     return tasklyst
 
 
-def showTask(tasklyst):
+#Need repair
+def removeTask(editNum, tasklyst):
+    
+    if editNum == len(tasklyst):
+        
+        tasklyst.pop()
+        return tasklyst
+    
+    else:
+        for i in range(len(tasklyst)):
+            
+            if i >= editNum-1 and i < len(tasklyst)-1:
+                tasklyst[i] = tasklyst[i+1]
+
+        tasklyst.pop()
+
+        return tasklyst
+
+
+def noTask(tasklyst):
 
     if len(tasklyst) == 0:
-        print("You have no task now.")
-
+        return True
+    
     else:
-        for i in tasklyst:
-            print(i)
+        return False
